7dc3d038b8b5c995a9adf45ffe7c5264007f5fcf
Elias Fang <eliasfang@gmail.com>
2021-03-02 10:51:45 -0800

    Add record form-control styling

--- public/js/record.js
+++ public/js/record.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 	$(".alert").hide();
 	$("#date").datepicker();
 	$("#clear").click(clear);
-	$("#save").click(save);
 });
 
 function clear() {
@@ -11,10 +10,4 @@ function clear() {
 		$("#date").val("");
 		$("#input").val("");
 	}
-}
-
-function save() {
-	$("#title").val("");
-	$("#date").val("");
-	$("#input").val("");
 }
