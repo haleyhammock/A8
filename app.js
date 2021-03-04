664ba5b1044f80fc77174739292ad7f352efc168
Elias Fang <eliasfang@gmail.com>
2021-03-03 22:25:10 -0800

    Fix record adding

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.get("/addRecord",addRecord.addRecord);
 app.get("/addPost",addPost.post);
 app.get("/forumLink", forumLink.link);
 app.get("/deletePosts2", deletePosts2.delete);
-app.get('routes/addRecords.js', function (req, res) {
+app.get('routes/addRecord.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'login-file.js'));
 });
 http.createServer(app).listen(app.get('port'), function(){
--- routes/addRecord.js
+++ routes/addRecord.js
@@ -1,25 +1,22 @@
 var data = require("../data.json");
 
 exports.addRecord = function(req, res) { 
-	if(req.query.sleep != ''){
-		var title = req.query.sleep;
-		var ques = "How many hours did you sleep last night?";
-	}
-	if(req.query.moody != ''){
-		var title = req.query.moody;
-		var ques = "On a scale of 1 to 10 how happy were you yesterday?";
-	}
-	if(req.query.exercise != ''){
-		var title = req.query.exercise;
-		var ques = "How many hours of excercise did you get yesterday?";
-	}
+	// if(req.query.sleep != ''){
+	// 	var ques = "How many hours did you sleep last night?";
+	// }
+	// if(req.query.moody != ''){
+	// 	var ques = "On a scale of 1 to 10 how happy were you yesterday?";
+	// }
+	// if(req.query.exercise != ''){
+	// 	var ques = "How many hours of excercise did you get yesterday?";
+	// }
 	var newRecord = {
 		"date": req.query.date,
 		"title": req.query.title,
 		"subject": req.query.subject,
-		"question": "Question",
 		"input": req.query.input
 	};
-	data.records.push(newRecord);
+	console.log(newRecord);
+	data.data.records.push(newRecord);
 	res.render("record", data);
 }
